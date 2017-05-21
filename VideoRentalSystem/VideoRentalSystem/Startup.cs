72e16caa8a78f8eec94c12d039ffd18f582782d0
Dimitar Pelev <dimitarpelev@gmail.com>
2017-05-21 14:10:01 +0300

    init project

---
+++ VideoRentalSystem/VideoRentalSystem/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoRentalSystem
+{
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
