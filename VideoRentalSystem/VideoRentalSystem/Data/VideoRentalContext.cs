7549c3cd1ebe88f99112f38535baa1c68e7b1fca
Krasimir Tsvetanov <kvtsvetanov@gmail.com>
2017-06-02 21:17:42 +0300

    init code first database

--- VideoRentalSystem/VideoRentalSystem/Data/VideoRentalContext.cs
+++ VideoRentalSystem/VideoRentalSystem/Data/VideoRentalContext.cs
@@ -5,6 +5,12 @@ namespace VideoRentalSystem.Data
 {
     public class VideoRentalContext : DbContext
     {
+        public VideoRentalContext()
+            :base("VideoRentalDBConnection")
+        {
+
+        }
+
         public DbSet<Country> CountriesTable { get; set; }
 
         public DbSet<Employee> EmployeesTable { get; set; }
