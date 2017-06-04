1c36a934b64e134a708067647de9645dc72cb8f4
Dimitar Pelev <dimitarpelev@gmail.com>
2017-06-04 12:13:07 +0300

    add Award toString

--- VideoRentalSystem/VideoRentalSystem/Models/Award.cs
+++ VideoRentalSystem/VideoRentalSystem/Models/Award.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace VideoRentalSystem.Models
 {
@@ -25,5 +26,17 @@ namespace VideoRentalSystem.Models
         // TODO: organisations
         // public Organisation  Organisation { get; set; }
         public bool IsDeleted { get; set; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.Append("Award: ");
+            sb.AppendLine(this.Name);
+            sb.Append("Date: ");
+            sb.AppendLine(this.Date.ToString());
+
+            return sb.ToString();
+        }
     }
 }
