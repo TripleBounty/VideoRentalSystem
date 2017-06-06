e1ecb818aaf2bb6b6ef10dc348de5ba9b42381c0
Krasimir Tsvetanov <kvtsvetanov@gmail.com>
2017-06-06 21:44:20 +0300

    Add postgre repository

---
+++ VideoRentalSystem/VideoRentalSystem/Data/Repository/TarifRepository.cs
@@ -0,0 +1,19 @@
+﻿using VideoRentalSystem.Data.Postgre;
+using VideoRentalSystem.Data.Postgre.Contracts;
+using VideoRentalSystem.Models;
+
+namespace VideoRentalSystem.Data.Repository
+{
+    public class TarifRepository : Repository<Tarif>, ITarifRepository
+    {
+        public TarifRepository(VideoRentalLoanContext context)
+            : base(context)
+        {
+        }
+
+        private VideoRentalLoanContext VideoRentalLoanContext
+        {
+            get { return this.Context as VideoRentalLoanContext; }
+        }
+    }
+}
