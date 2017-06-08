78fd40c5f1e9817afda5384f725de6f0ea7b291e
Krasimir Tsvetanov <kvtsvetanov@gmail.com>
2017-06-08 23:08:15 +0300

    fix migration problems

--- VideoRentalSystem/VideoRentalSystem/Migrations/VideoRentalContext/201706082000516_init.Designer.cs
+++ VideoRentalSystem/VideoRentalSystem/Migrations/VideoRentalContext/201706082000516_init.Designer.cs
@@ -7,13 +7,13 @@ namespace VideoRentalSystem.Migrations.VideoRentalContext
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class Customers23 : IMigrationMetadata
+    public sealed partial class init : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(Customers23));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(init));
         
         string IMigrationMetadata.Id
         {
-            get { return "201706072011133_Customers23"; }
+            get { return "201706082000516_init"; }
         }
         
         string IMigrationMetadata.Source
