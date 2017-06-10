26539dc0e2876491bd602289eec06eb6be363d94
Krasimir Tsvetanov <kvtsvetanov@gmail.com>
2017-06-10 11:51:32 +0300

    Add RemoveFilmQuantityCommand tests

--- VideoRentalSystem/VideoRentalSystem/Commands/RemoveCommands/RemoveFilmQuantityCommand.cs
+++ VideoRentalSystem/VideoRentalSystem/Commands/RemoveCommands/RemoveFilmQuantityCommand.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using VideoRentalSystem.Commands.Contracts;
 using VideoRentalSystem.Data.Contracts;
-using VideoRentalSystem.Models.Factories;
 
 namespace VideoRentalSystem.Commands.RemoveCommands
 {
@@ -12,6 +12,11 @@ namespace VideoRentalSystem.Commands.RemoveCommands
 
         public RemoveFilmQuantityCommand(IDatabase db)
         {
+            if (db == null)
+            {
+                throw new ArgumentNullException("Invalid database in AddFilmQuantityCommand constructor");
+            }
+
             this.db = db;
         }
 
