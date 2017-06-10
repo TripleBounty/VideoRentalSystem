07269c7546455ac773ee301b3a7498c3732a3188
Krasimir Tsvetanov <kvtsvetanov@gmail.com>
2017-06-10 11:34:57 +0300

    Add AddFilmStaffCommand and AddStoreEmployeeCommand Tests of Constructors

---
+++ VideoRentalSystem/VideoRentalSystem.Tests/Commands/AddCommands/AddFilmStaffCommand_ConstructorShould.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using VideoRentalSystem.Commands.AddCommands;
+using VideoRentalSystem.Data.Contracts;
+
+namespace VideoRentalSystem.Tests.Commands.AddCommands
+{
+    [TestFixture]
+    public class AddFilmStaffCommand_ConstructorShould
+    {
+        [Test]
+        public void ThrowException_WhenInvalidParameterIsPassed()
+        {
+            //Arrange, Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new AddFilmQuantityCommand(null));
+        }
+
+        [Test]
+        public void NotThrowException_WhenInvalidParameterIsPassed()
+        {
+            //Arrange 
+            var dbMock = new Mock<IDatabase>();
+
+            //Act & Assert
+            Assert.DoesNotThrow(() => new AddFilmQuantityCommand(dbMock.Object));
+        }
+    }
+}
