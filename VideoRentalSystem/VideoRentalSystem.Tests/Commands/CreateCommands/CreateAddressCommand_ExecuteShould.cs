492c0f2b9494082e77abeaa1592d6687dde83a6e
Krasimir Tsvetanov <kvtsvetanov@gmail.com>
2017-06-10 18:24:05 +0300

    Add CreateCountryCommand Tests

--- VideoRentalSystem/VideoRentalSystem.Tests/Commands/CreateCommands/CreateAddressCommand_ExecuteShould.cs
+++ VideoRentalSystem/VideoRentalSystem.Tests/Commands/CreateCommands/CreateAddressCommand_ExecuteShould.cs
@@ -2,10 +2,7 @@
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 using VideoRentalSystem.Commands.CreateCommands;
 using VideoRentalSystem.Data.Contracts;
 using VideoRentalSystem.Models;
---
+++ VideoRentalSystem/VideoRentalSystem.Tests/Commands/CreateCommands/CreateCountryCommand_ConstructorShould.cs
@@ -0,0 +1,44 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using VideoRentalSystem.Commands.CreateCommands;
+using VideoRentalSystem.Data.Contracts;
+using VideoRentalSystem.Models.Factories;
+
+namespace VideoRentalSystem.Tests.Commands.CreateCommands
+{
+    [TestFixture]
+    public class CreateCountryCommand_ConstructorShould
+    {
+        [Test]
+        public void ThrowArgumentNullException_WhenInvalidDbParameterIsPassed()
+        {
+            //Arrange
+            var factoryMock = new Mock<IModelsFactory>();
+
+            //Act & Arrange
+            Assert.Throws<ArgumentNullException>(() => new CreateCountryCommand(null, factoryMock.Object));
+        }
+
+        [Test]
+        public void ThrowArgumentNullException_WhenInvalidFactoryParameterIsPassed()
+        {
+            //Arrange
+            var dbMock = new Mock<IDatabase>();
+
+            //Act & Arrange
+            Assert.Throws<ArgumentNullException>(() => new CreateCountryCommand(dbMock.Object, null));
+        }
+
+        [Test]
+        public void NotThrow_WhenInvalidFactoryParameterIsPassed()
+        {
+            //Arrange
+            var dbMock = new Mock<IDatabase>();
+            var factoryMock = new Mock<IModelsFactory>();
+
+            //Act & Arrange
+            Assert.DoesNotThrow(() => new CreateCountryCommand(dbMock.Object, factoryMock.Object));
+        }
+    }
+}
