deff3630efcdbcc160b27317db78742e3e5b38fc
Dimitar Pelev <dimitarpelev@gmail.com>
2017-06-13 12:18:32 +0300

    GetFilmScore AverageFilmScoreFromAllReviews test

--- VideoRentalSystem/VideoRentalSystem.Tests/Common/GetFilmScore_GetAvgScoreShould.cs
+++ VideoRentalSystem/VideoRentalSystem.Tests/Common/GetFilmScore_GetAvgScoreShould.cs
@@ -3,10 +3,12 @@ using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using VideoRentalSystem.Common;
 using VideoRentalSystem.Data.Contracts;
+using VideoRentalSystem.Data.Repository.Contracts;
 using VideoRentalSystem.Models;
 
 namespace VideoRentalSystem.Tests.Common
@@ -41,18 +43,48 @@ namespace VideoRentalSystem.Tests.Common
             Assert.Throws<ArgumentException>(() => sut.GetAvgFilmScore(validFilmName));
         }
 
-        //[Test]
+        [Test]
         public void ShouldReturn_AverageFilmScoreFromAllReviews()
         {
             //Arrange
             var validFilmName = "validFilmName";
 
-            var databaseMock = new Mock<IDatabase>();
             var filmMock = new Mock<Film>();
-            filmMock.SetupGet(x => x.Id).Returns(1);
+            filmMock.Object.Id = 1;
+            filmMock.Object.Name = validFilmName;
 
-            var reviewsMock = new Mock<Review>();
-            databaseMock.Setup(x => x.Films.SingleOrDefault(y => y.Name == validFilmName)).Returns<Film>(null);
+            var review1Mock = new Mock<Review>();
+            review1Mock.SetupGet(x => x.Film).Returns(filmMock.Object);
+            review1Mock.Object.Rating = 3.5;
+
+            var review2Mock = new Mock<Review>();
+            review2Mock.SetupGet(x => x.Film).Returns(filmMock.Object);
+            review2Mock.Object.Rating = 7.5;
+
+            var review3Mock = new Mock<Review>();
+            review3Mock.SetupGet(x => x.Film).Returns(filmMock.Object);
+            review3Mock.Object.Rating = 4.0;
+
+            var reviewCollection = new HashSet<Review>();
+            reviewCollection.Add(review1Mock.Object);
+            reviewCollection.Add(review2Mock.Object);
+            reviewCollection.Add(review3Mock.Object);
+
+            var reviewRepository = new Mock<IReviewRepository>();
+            reviewRepository.Setup(x => x.GetAll()).Returns(reviewCollection);
+
+            var databaseMock = new Mock<IDatabase>();
+            databaseMock.SetupGet(x => x.Reviews).Returns(reviewRepository.Object);
+
+            databaseMock.Setup(x => x.Films.SingleOrDefault(It.IsAny<Expression<Func<Film, bool>>>())).Returns(filmMock.Object);
+
+            var sut = new GetFilmScore(databaseMock.Object);
+
+            //Act
+            var result = sut.GetAvgFilmScore(validFilmName);
+
+            //Assert
+            Assert.AreEqual(5.0, result);
         }
     }
 }
